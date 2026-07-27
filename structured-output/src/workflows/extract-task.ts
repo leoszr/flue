8acee3bb0c5c734089cfed87b867bf4b7b466d56
leonardo <leo.srodrigues01@gmail.com>
2026-07-27 20:35:17 -0300

    Update extract-task.ts

--- structured-output/src/workflows/extract-task.ts
+++ structured-output/src/workflows/extract-task.ts
@@ -6,4 +6,11 @@ const inputSchema = v.object({
   text: v.pipe(v.string(), v.minLength(1), v.maxLength(2000))
 })
 
+const taskSchema = v.object({
+  title: v.pipe(v.string(), v.minLength(1), v.maxLength(2000)),
+  priority: v.picklist(["low", "medium", "high"]),
+  asignee: v.nullable(v.string(), "teste"),
+  dueDate: v.nullable(v.date(), () => new Date())
+})
+
 export default defineWorkflow({})
